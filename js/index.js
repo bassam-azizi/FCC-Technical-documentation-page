e89294a184b9bb1bf0dfbbb52e3ec83f4c6469d9
bassem <101010bassam@gmail.com>
2020-05-21 15:49:54 +0100

    on scroll, navigations are opacity zero function created

--- js/index.js
+++ js/index.js
@@ -1,3 +1,17 @@
+// -------------- HEADER AND NAVIGATION DISPLAY NONE WHEN SCROLL DOWN ----------------------------
+let initial_scroll_position = window.pageYOffset;
+window.onscroll = function () {
+    let currentScrollPos = window.pageYOffset;
+    if (initial_scroll_position != currentScrollPos) {
+        this.document.getElementById('title').style.opacity = '0';
+        this.document.getElementById('hamburger').style.opacity = '0';
+    } else {
+        this.document.getElementById('title').style.opacity = '1';
+        this.document.getElementById('hamburger').style.opacity = '1';
+    }
+}
+
+// ------------- NAV BAR ITEMS 'S CURRENT SELECTIONS --------------------
 let nav_link_node = document.getElementsByClassName('nav-link');
 let nav_links = Array.from(nav_link_node);
 
