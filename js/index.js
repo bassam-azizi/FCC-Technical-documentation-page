82707f90b69204d403b5d580ea76f880bd7652ec
bassem <101010bassam@gmail.com>
2020-06-01 18:36:04 +0100

    cleaning and remove the old path, new working path is seted

--- js/index.js
+++ js/index.js
@@ -36,12 +36,56 @@ window.onscroll = function () {
 // ------------- NAV BAR ITEMS 'S CURRENT SELECTIONS --------------------
 let nav_link_node = document.getElementsByClassName('nav-link');
 let nav_links = Array.from(nav_link_node);
-let introduction = document.getElementById('Introduction');
 
+let introduction = document.getElementById('Introduction');
+// let main_sectionHT = document.getElementsByTagName('section');
+// let main_sections = Array.from(main_sectionHT);
+
+// for (i = 1; i < nav_links.length; i++) {
+//     if (!nav_links[i].classList.contains('current')) {
+//         nav_links[0].classList.add('current');
+//         console.log('brick does not contain ');
+//     }
+//     else {
+//         nav_links[0].classList.remove('current');
+//     }
+// }
+// nav_links[0].classList.add('current');
+// if (nav_links[0].classList.contains('current')) {
+//     introduction.style.display = "block";
+// }
+// else {
+//     introduction.style.display = 'none';
+// }
+
+// ---------------------     ---------------------------------------
+// function current(e) {
+//     e.target.classList.add('current');
+//     console.log(e.target);
+// }
+
+// sideBar.addEventListener('click', current);
 nav_links.forEach(nav_link => {
     nav_link.addEventListener('click', function () {
-        if (nav_link.innerHTML !== 'Introduction') { console.log(nav_link.innerHTML); introduction.style.display = "none"; }
-        nav_links.forEach(nav_link => nav_link.classList.remove('current'));
-        nav_link.classList.add('current');
+        // window.addEventListener('hashchange', e => {
+        //     if (window.location.href.split('#')[1] === nav_link.innerHTML) {
+        //         nav_link.classList.add('current');
+        //         console.log('this done');
+        //     } else {
+        //         nav_link.classList.remove('current');
+        //     }
+        // });
+
+        // if (nav_link.innerHTML == 'Introduction') {
+        //     introduction.style.display = "block";
+        // }
+        // else {
+        //     introduction.style.display = "none";
+        // }
+        // nav_links.forEach(nav_link => nav_link.classList.remove('current'));
+        // nav_link.classList.add('current');
+        console.log(nav_link.innerHTML);
+        window.addEventListener('hashchange', e => { console.log(window.location.href.split('#')[1]) });
+        // console.log(hash());
     })
 })
